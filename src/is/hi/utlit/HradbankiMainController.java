29394307ab276aca35dfeb612a38c4a0883d1075
Styrmir Óli Þorsteinsson <styrmir97@gmail.com>
2019-03-29 11:36:14 +0000

    Fixing some issus with the controllers
    have combined 3 controller into one controller as it is more effective

--- src/is/hi/utlit/HradbankiMainController.java
+++ src/is/hi/utlit/HradbankiMainController.java
@@ -23,16 +23,47 @@ public class HradbankiMainController implements Initializable {
 
     @FXML
     private TextField PinId;
-    @FXML
-    private HradBankiKeybordController hradBankiKeybordController;
     private int[] pinArray;
     private String currentPin;
+    @FXML
+    private Button KeyBord_1;
+    @FXML
+    private Button KeyBord_4;
+    @FXML
+    private Button KeyBord_7;
+    @FXML
+    private Button KeyBord_10;
+    @FXML
+    private Button KeyBord_2;
+    @FXML
+    private Button KeyBord_5;
+    @FXML
+    private Button KeyBord_8;
+    @FXML
+    private Button KeyBord_0;
+    @FXML
+    private Button KeyBord_3;
+    @FXML
+    private Button KeyBord_6;
+    @FXML
+    private Button KeyBord_9;
+    @FXML
+    private Button KeyBord_11;
+    @FXML
+    private Button KeyBord_Enter;
+    @FXML
+    private Button KeyBord_Cancel;
+    @FXML
+    private Button KeyBord_Quit;
+    
+    private int[] numberArray = new int[4];
+    private int pinSum;
+    private int selectedButton;
+    private int currentIndex;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        //hradBankiKeybordController = new HradBankiKeybordController();
-        hradBankiKeybordController.getKeybord(this);
     }    
 
     @FXML
@@ -49,8 +80,44 @@ public class HradbankiMainController implements Initializable {
     }
     */
     public void displayPin() {
-        currentPin += hradBankiKeybordController.DisplayNumber();
         System.out.println("Current:"+currentPin);
         PinId.setText(currentPin);
+    }
+
+    @FXML
+    private void enterHandler(ActionEvent event) {
+        if(currentIndex <= 4) {
+            
+        }
+    }
+
+    @FXML
+    private void cancelHandler(ActionEvent event) {
+        if (currentIndex >= 1) {
+            numberArray[currentIndex] = 0;
+            currentIndex -= 1;
+        }
+    }
+
+    @FXML
+    private void quitEnter(ActionEvent event) {
+    }
+
+    @FXML
+    private void numberHandler(ActionEvent event) {
+        if (currentIndex < 4) {
+            Button button = (Button)event.getSource();
+            int id = Integer.parseInt(button.getText());
+            pinSum += (id*1000)/Math.pow(10, currentIndex);
+            System.out.println(pinSum);
+            numberArray[currentIndex] = id;
+            currentIndex += 1;
+            for(int i = 0; i < currentIndex; i++) {
+                System.out.print(numberArray[i]+",");
+            }
+            System.out.println();
+        } else {
+            System.out.println("Error");
+        }
     }
 }
